bc45d0b070d656aa1c4502575b2e2ae033409377
Santosh Gadkari <gadkari-santosh@users.noreply.github.com>
2023-11-29 12:42:02 +0000

    added PagedData

---
+++ src/main/java/com/nitome/dto/PagedData.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023,2024,2025 Neetome, All rights reserved
+ *
+ * Licensed under the Neetome, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * Reach out to Neetome organization to obtain copy of the license.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed, copying & reusing file in STRICTLY PROHIBITED.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nitome.dto;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.List;
+
+@Value
+@Builder
+public class PagedData<T> {
+
+    long totalElements;
+
+    int totalPages;
+
+    long number;
+
+    long numberOfElements;
+
+    List<T> content;
+}
